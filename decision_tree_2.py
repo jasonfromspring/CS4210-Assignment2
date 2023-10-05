bddbd946de5504a80af7a7ee2011b29dc2816073
Jason Phan <99059033+jasonfromspring@users.noreply.github.com>
2023-10-05 06:06:26 -0700

    Add files via upload

--- decision_tree_2.py
+++ decision_tree_2.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Jason Phan
+# FILENAME: decision_tree_2.py
+# SPECIFICATION: View performance of contact lens training and test
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 1 hr
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
